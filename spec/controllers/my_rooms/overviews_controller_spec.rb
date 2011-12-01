545cd20d29e4dcd28bc2cf7b6d1cdd2bef2c6b3f
David Henner <drhenner@yahoo.com>
2011-12-01 18:40:36 -0500

    need to login to view

--- spec/controllers/my_rooms/overviews_controller_spec.rb
+++ spec/controllers/my_rooms/overviews_controller_spec.rb
@@ -3,6 +3,13 @@ require  'spec_helper'
 describe MyRooms::OverviewsController do
   render_views
 
+  before(:each) do
+    activate_authlogic
+
+    @user = Factory(:user)
+    login_as(@user)
+  end
+
   it "show action should render show template" do
     get :show
     response.should render_template(:show)
