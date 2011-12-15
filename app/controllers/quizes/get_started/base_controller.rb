3d326f5806cc1f6c7d19ce65f15fe4ce3a1534e4
David Henner <drhenner@yahoo.com>
2011-12-15 18:23:43 -0500

    posgres didnt like syntax

--- app/controllers/quizes/get_started/base_controller.rb
+++ app/controllers/quizes/get_started/base_controller.rb
@@ -3,7 +3,7 @@ class Quizes::GetStarted::BaseController < Quizes::BaseController
 
   private
   def next_question
-    questionnaire       = Questionnaire.includes(:questions).order(:question => :position).find(1)
+    questionnaire       = Questionnaire.find(1)
     remaining_questions = (questionnaire.active_question_ids - current_user.user_answers.map(&:question_id))
     remaining_questions.empty? ? nil : questionnaire.questions.find(remaining_questions.first)
   end
--- app/models/questionnaire.rb
+++ app/models/questionnaire.rb
@@ -1,4 +1,4 @@
 class Questionnaire < ActiveRecord::Base
   has_many :questions
-  has_many :active_questions, :class_name => 'Question', :conditions => ['questions.active = ?', true]
+  has_many :active_questions, :class_name => 'Question', :conditions => ['questions.active = ?', true], :order => 'questions.position ASC'
 end
